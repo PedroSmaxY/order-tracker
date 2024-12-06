fa97647033ef550d3557860c85cdb43a3ddb338d
PedroSmaxY <pedrohnovais@outlook.com>
2024-12-06 17:12:45 -0300

    feat: add OrderService

---
+++ order-service/src/main/java/com/pedrosmaxy/order_service/service/OrderService.java
@@ -0,0 +1,55 @@
+package com.pedrosmaxy.order_service.service;
+
+import com.pedrosmaxy.order_service.DTO.OrderRequestDTO;
+import com.pedrosmaxy.order_service.domain.order.Order;
+import com.pedrosmaxy.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final KafkaTemplate<UUID, String> kafkaTemplate;
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Order getOrderById(UUID id) {
+        return orderRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Order not found"));
+    }
+
+    public Order createOrder(OrderRequestDTO orderRequestDTO) {
+        Order order = new Order();
+        order.setId(UUID.randomUUID());
+        order.setUser(orderRequestDTO.user());
+        order.setItems(orderRequestDTO.items());
+        order.setCreatedAt(new Date());
+        order.setUpdatedAt(new Date());
+        order.setStatus(orderRequestDTO.status());
+
+        kafkaTemplate.send("order-events", order.getId(), "CREATED");
+
+        return orderRepository.save(order);
+    }
+
+    public Order updateOrder(UUID id, OrderRequestDTO orderRequestDTO) {
+        Order order = getOrderById(id);
+        order.setUser(orderRequestDTO.user());
+        order.setItems(orderRequestDTO.items());
+        order.setUpdatedAt(new Date());
+        order.setStatus(orderRequestDTO.status());
+        return orderRepository.save(order);
+    }
+
+    public void deleteOrder(UUID id) {
+        orderRepository.deleteById(id);
+    }
+}
