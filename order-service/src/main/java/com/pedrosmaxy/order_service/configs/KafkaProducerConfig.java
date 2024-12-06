0acd4f650a460d63736e4192075e89ea5a339e29
PedroSmaxY <pedrohnovais@outlook.com>
2024-12-06 17:39:03 -0300

    fix: correct endpoints request body

--- order-service/src/main/java/com/pedrosmaxy/order_service/configs/KafkaProducerConfig.java
+++ order-service/src/main/java/com/pedrosmaxy/order_service/configs/KafkaProducerConfig.java
@@ -2,6 +2,7 @@ package com.pedrosmaxy.order_service.configs;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.UUIDSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -18,7 +19,7 @@ public class KafkaProducerConfig {
     public ProducerFactory<UUID, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, UUIDSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
